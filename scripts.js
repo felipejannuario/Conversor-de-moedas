dcc50d43891a24156fd6884b5e2f3bc1a9792a43
Felipe Januario <felipejanuarioalves@outlook.com>
2025-05-10 21:12:16 -0300

    alteração 10/05

--- scripts.js
+++ scripts.js
@@ -44,41 +44,87 @@ function updateCurrencyImage(selectElement, imageElement, textElement) {
     }
 }
 
-function convertValues() {
-    const inputCurrencyValue = parseFloat(document.querySelector('.input-currency').value.replace(/[^0-9,.-]+/g, '').replace(',', '.'));
+// function convertValues() {
+//     const inputCurrencyValue = parseFloat(document.querySelector('.input-currency').value.replace(/[^0-9,.-]+/g, '').replace(',', '.'));
+//     const currencySelectFrom = document.querySelector('.currency-select-from');
+//     const currencySelectTo = document.querySelector('.currency-select-to');
+//     const currencyValueToConvert = document.querySelector('.currency-value-to-convert');
+//     const currencyValueConverted = document.querySelector('.currency-value');
+
+//     // Taxas de câmbio fixas (exemplo)
+//     const exchangeRates = {
+//         real: 1,        // 1 Real = 1 Real
+//         dolar: 5.66,    // 1 Real = 5.66 Dólares
+//         euro: 6.7,      // 1 Real = 6.7 Euros
+//         libra: 7.15,    // 1 Real = 7.15 Libras
+//         bitcoin: 355517.37, // 1 Real = 355517.37 Bitcoins
+//     };
+
+//     if (isNaN(inputCurrencyValue)) {
+//         alert('Por favor, insira um valor válido.');
+//         return;
+//     }
+
+//     const fromRate = exchangeRates[currencySelectFrom.value];
+//     const toRate = exchangeRates[currencySelectTo.value];
+//     const convertedValue = (inputCurrencyValue * fromRate) / toRate;
+
+//     currencyValueConverted.innerHTML = new Intl.NumberFormat(getLocale(currencySelectTo.value), {
+//         style: 'currency',
+//         currency: getCurrencyCode(currencySelectTo.value)
+//     }).format(convertedValue);
+
+//     currencyValueToConvert.innerHTML = new Intl.NumberFormat(getLocale(currencySelectFrom.value), {
+//         style: 'currency',
+//         currency: getCurrencyCode(currencySelectFrom.value)
+//     }).format(inputCurrencyValue);
+// }
+
+async function convertValues() {
+    const inputValueRaw = document.querySelector('.input-currency').value;
+    const inputCurrencyValue = parseFloat(inputValueRaw.replace(/[^0-9,.-]+/g, '').replace(',', '.'));
     const currencySelectFrom = document.querySelector('.currency-select-from');
     const currencySelectTo = document.querySelector('.currency-select-to');
     const currencyValueToConvert = document.querySelector('.currency-value-to-convert');
     const currencyValueConverted = document.querySelector('.currency-value');
-
-    // Taxas de câmbio fixas (exemplo)
-    const exchangeRates = {
-        real: 1,        // 1 Real = 1 Real
-        dolar: 5.66,    // 1 Real = 5.66 Dólares
-        euro: 6.7,      // 1 Real = 6.7 Euros
-        libra: 7.15,    // 1 Real = 7.15 Libras
-        bitcoin: 355517.37, // 1 Real = 355517.37 Bitcoins
-    };
 
     if (isNaN(inputCurrencyValue)) {
         alert('Por favor, insira um valor válido.');
         return;
     }
 
-    const fromRate = exchangeRates[currencySelectFrom.value];
-    const toRate = exchangeRates[currencySelectTo.value];
-    const convertedValue = (inputCurrencyValue * fromRate) / toRate;
+    const fromCurrencyCode = getCurrencyCode(currencySelectFrom.value);
+    const toCurrencyCode = getCurrencyCode(currencySelectTo.value);
 
-    currencyValueConverted.innerHTML = new Intl.NumberFormat(getLocale(currencySelectTo.value), {
-        style: 'currency',
-        currency: getCurrencyCode(currencySelectTo.value)
-    }).format(convertedValue);
+    // Adicionando a chave de API
+    const url = `https://api.exchangerate.host/convert?from=${fromCurrencyCode}&to=${toCurrencyCode}&amount=${inputCurrencyValue}&access_key=c9e230dfdce933d1984463b97bc8e5f3`;
 
-    currencyValueToConvert.innerHTML = new Intl.NumberFormat(getLocale(currencySelectFrom.value), {
-        style: 'currency',
-        currency: getCurrencyCode(currencySelectFrom.value)
-    }).format(inputCurrencyValue);
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+
+        if (!data.result) {
+            throw new Error('Erro ao obter taxa de câmbio.');
+        }
+
+        const convertedValue = data.result;
+
+        currencyValueConverted.innerHTML = new Intl.NumberFormat(getLocale(currencySelectTo.value), {
+            style: 'currency',
+            currency: toCurrencyCode
+        }).format(convertedValue);
+
+        currencyValueToConvert.innerHTML = new Intl.NumberFormat(getLocale(currencySelectFrom.value), {
+            style: 'currency',
+            currency: fromCurrencyCode
+        }).format(inputCurrencyValue);
+
+    } catch (error) {
+        console.error('Erro ao converter moeda:', error);
+        alert('Ocorreu um erro ao converter a moeda. Tente novamente mais tarde.');
+    }
 }
+
 
 function getLocale(currency) {
     switch (currency) {
